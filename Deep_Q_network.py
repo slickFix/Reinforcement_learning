8b3ac8fe23aafc887cf395717413419fe9444966
slickFix <sidd.200894@gmail.com>
2019-04-15 16:32:49 +0530

    defining CNN network architecture

--- Deep_Q_network.py
+++ Deep_Q_network.py
@@ -7,6 +7,11 @@ Created on Sat Apr 13 13:46:32 2019
 """
 
 import gym
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib.layers import convolution2d,fully_connected
+
+
 env = gym.make("MsPacman-v0")
 obs = env.reset()
 obs.shape
@@ -20,3 +25,18 @@ def preprocess_observation(obs):
     img[img  == mspacman_color] = 0 # improve contrast
     img = (img-128)/128-1 # normalize from -1 to 1
     return img.reshape(88,80,1)
+
+input_height = 88
+input_width = 80
+input_channels = 1
+conv_n_maps = [32,64,64]
+conv_kernel_sizes = [(8,8),(4,4),(3,3)]
+conv_strides = [4,2,1]
+conv_paddings = ["SAME"]*3
+conv_activation = [tf.nn.relu]*3
+n_hidden_in = 64*11*10 # conv3  has 64 maps of 11*10 each
+n_hidden = 512
+
+hidden_activation = tf.nn.relu
+n_outputs = env.action_space.n  # 9 discrete actions are available
+initializer = tf.contrib.layers.variance_scaling_initializer()
